326602f9b9ce6cef288b56d97f4e89e616406bf2
Eric <ericbradshaw98@gmail.com>
2024-01-08 20:06:19 -0500

    created file

---
+++ tail.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const tail = function(array) {
+  return array.slice(1);
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
+
+console.log(tail(["a", "b", "c"]));
+console.log(tail([]));
+console.log(tail([42]));
