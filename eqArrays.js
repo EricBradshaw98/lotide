d97df6f10a35c28e62dcbd8719dced5fb5b19737
Eric <ericbradshaw98@gmail.com>
2024-01-09 19:33:51 -0500

    modified again with suggested changes

--- eqArrays.js
+++ eqArrays.js
@@ -27,8 +27,8 @@ const assertEqual = function(actual, expected) {
 };
 
 // Test cases
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), false); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should PASS
