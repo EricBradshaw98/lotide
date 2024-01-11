036299b7c81558a6c550209b35b0ae9c5f75c78c
Eric <ericbradshaw98@gmail.com>
2024-01-10 21:49:17 -0500

    updated file

---
+++ letterPositions.js
@@ -0,0 +1,48 @@
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+
+  for (let i = 0; i < sentence.length; i++) {
+    const char = sentence[i].toLowerCase();
+
+    if (char !== ' ') {
+      if (results[char]) {
+        results[char].push(i);
+      } else {
+        results[char] = [i];
+      }
+    }
+  }
+
+  return results;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  const result = eqArrays(arr1, arr2);
+  if (result) {
+    console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
+// Test cases for letterPositions with the test string "hello"
+const results = letterPositions("hello");
+
+assertArraysEqual(results['h'], [0]);
+assertArraysEqual(results['e'], [1]);
+assertArraysEqual(results['l'], [2, 3]);
+assertArraysEqual(results['o'], [4]);
