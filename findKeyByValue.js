bd38d231565340a441396ef1f568eb8e8070677c
Eric <ericbradshaw98@gmail.com>
2024-01-10 22:47:05 -0500

    created file

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+
+//define findkeybyvalue object
+const findKeyByValue = function(object, value) {
+  for (const key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+};
+//assertequal defined  to verify actual and expected values
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✅✅✅ Assertion Passed: ' + actual + ' === ' + expected + '');
+  } else {
+    console.log('🛑🛑🛑 Assertion Failed: ' + actual + ' !== ' + expected + '');
+  }
+};
+//contains genre info
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+
+//assertequal 
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
