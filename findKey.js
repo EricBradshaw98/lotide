569b9170b9259fc99d9d8bc0122925a739661780
Eric <ericbradshaw98@gmail.com>
2024-01-11 21:35:45 -0500

    created and uploaded

---
+++ findKey.js
@@ -0,0 +1,44 @@
+//define findkey function to search fo key in object and return undefined for keys not found
+
+const findKey = function(object, callback) {
+  for (const key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+// define result and integrate findkey function
+const result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2);
+//imported function from other file
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+//adjusted original function
+const assertArraysEqual = function(arr1, arr2) {
+  const result = eqArrays(arr1, arr2);
+  if (result) {
+    console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
+// Corrected test case
+assertArraysEqual([result], ["noma"]);
