528ba50d2e266c9401ac4448e43867e12ae3e60b
Eric <ericbradshaw98@gmail.com>
2024-01-15 13:47:16 -0500

    created test files

---
+++ test/headTest.js
@@ -0,0 +1,12 @@
+
+
+const head = require('../head.js');
+const assertEqual = require('../assertEqual');
+
+
+
+
+assertEqual(head([5, 6, 7]), 5); 
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
+assertEqual(head([]), undefined); 
+assertEqual(head([87]), 87); 
